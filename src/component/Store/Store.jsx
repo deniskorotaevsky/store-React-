0984929c726659f3b1cbe392d0cb0ecbf47a2dd6
Denis Korotaevsky <huligan8771@gmail.com>
2022-09-13 03:16:00 +0300

    store

--- src/component/Store/Store.jsx
+++ src/component/Store/Store.jsx
@@ -1,21 +1,40 @@
+import s from './Store.module.css';
+import Phone from './Phone/Phone';
+
 export const Store = (props) => {
     const { setPage } = props;
 
     return (
-        <>
+        <div className={s.store}>
             <header>
-                <nav className='nav'>
-                    <button onClick={() => setPage('home')}>
-                        Super страница
+                <nav>
+                    <button
+                        className={s.nav_botton}
+                        onClick={() => setPage('home')}
+                    >
+                        Главная страница
                     </button>
-                    <button onClick={() => setPage('login')}>
+                    <button
+                        className={s.nav_botton}
+                        onClick={() => setPage('login')}
+                    >
                         Авторизация
                     </button>
-                    <button onClick={() => setPage('store')}>Магазин</button>
+                    <button
+                        className={s.nav_botton}
+                        onClick={() => setPage('store')}
+                    >
+                        Магазин
+                    </button>
                 </nav>
             </header>
-            <p>ergerhgher</p>
-        </>
+            <div className={s.store_row}>
+                <Phone />
+                <Phone />
+                <Phone />
+                <Phone />
+            </div>
+        </div>
     );
 };
 
